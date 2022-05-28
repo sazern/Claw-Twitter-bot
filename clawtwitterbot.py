d942afcf853f005481288a0a5f5d1553df839d8f
sazern <83553077+sazern@users.noreply.github.com>
2022-05-28 18:11:03 +0200

    fixed issue with JsonDecoder and made it silent

--- clawtwitterbot.py
+++ clawtwitterbot.py
@@ -1,16 +1,18 @@
+import logging
+import gc
 import requests
 import json
-import os
 import time
 from iconsdk.icon_service import IconService
 from iconsdk.providers.http_provider import HTTPProvider
-import tweepy
-
 icon_service = IconService(HTTPProvider("https://ctz.solidwallet.io/api/v3"))
 claw_contract = "cx45cf0c108c3df650b1c28d9e2fdc8b4d068cb2fa"
+from datetime import datetime
 
 
-#LOGIN To twitter
+
+import tweepy
+
 client = tweepy.Client(consumer_key='',
                        consumer_secret='',
                        access_token='',
@@ -20,12 +22,12 @@ client = tweepy.Client(consumer_key='',
 
 EXA = 10**18
 
-#Get data from icon API of sales where claw contract = score_address
+
 def getlog():
-    craft_log = requests.get('https://tracker.icon.community/api/v1/logs?score_address=cx45cf0c108c3df650b1c28d9e2fdc8b4d068cb2fa&method=TransferSingle')
+    craft_log = requests.get('https://tracker.icon.community/api/v1/logs?score_address=cx45cf0c108c3df650b1c28d9e2fdc8b4d068cb2fa&method=TransferSingle&limit=1')
     logdata = craft_log.text
     json_logdata = json.loads(logdata)
-    latesttx = json_logdata[0] #latest row of data
+    latesttx = json_logdata[0]
     global txhash
     txhash = latesttx["transaction_hash"]
     indexed = latesttx["indexed"]
@@ -36,16 +38,13 @@ def getlog():
     global reciever
     reciever = json_indexed[3]
     
-    if operator == "cx9c4698411c6d9a780f605685153431dcda04609f": #Making sure its from craft contract == sale
+    if operator == "cx9c4698411c6d9a780f605685153431dcda04609f":
         sale = 1
     else:
          sale = 0
     
     if sale == 1:
-        print("### NFT SOLD ###")
-        print("From: ", seller)
-        print("To: ", reciever)
-        print("TxHash: ", txhash)
+        
         
         gettxresult()
         
@@ -55,56 +54,64 @@ def getlog():
 
     
     
-    #Get TX data of the latest sale
+    
 def gettxresult():
     txresult = icon_service.get_transaction_result(txhash)
     result2 = txresult["eventLogs"][4]
     result3 = txresult["eventLogs"][0]
-    hexamount = result3["indexed"][3] #Price not including fees
+    hexamount = result3["indexed"][3]
     exaamount = int(hexamount, 16)
     global amount
     amount = exaamount / EXA
-    print("Amount: ", amount, " ICX")
-    hextokenid = result2["data"][0] #Gets tokenid of NFT sold
+    
+    hextokenid = result2["data"][0]
     global tokenid
     tokenid = int(hextokenid, 16)
     global strtokenid
     strtokenid = str(tokenid)
-    print("TokenID: ", tokenid)
     global url
     url = "https://craft.network/nft/cx45cf0c108c3df650b1c28d9e2fdc8b4d068cb2fa:" + strtokenid
-    print(url)
     tweet()
     
+    
 
-#Checks TX Hash to find out if there was a new sale
+
 def check_old():
-    craft_log = requests.get('https://tracker.icon.community/api/v1/logs?score_address=cx45cf0c108c3df650b1c28d9e2fdc8b4d068cb2fa&method=TransferSingle')
+    
+    craft_log = requests.get('https://tracker.icon.community/api/v1/logs?score_address=cx45cf0c108c3df650b1c28d9e2fdc8b4d068cb2fa&method=TransferSingle&limit=1')
     logdata = craft_log.text
-    json_logdata = json.loads(logdata)
-    latesttx = json_logdata[0]
-    newtxhash = latesttx["transaction_hash"]
-    if newtxhash == txhash:
-        del craft_log
-        del logdata
-        del json_logdata
-        gc.collect()
+    try:
+     json_logdata = json.loads(logdata)
+    except json.decoder.JSONDecodeError:
+        now = datetime.now()
 
-        time.sleep(60)
+        current_time = now.strftime("%H:%M:%S")
+        print(current_time + " Error decoding JSON")
+        time.sleep(3)
         check_old()
     else:
-        getlog()
+        latesttx = json_logdata[0]
+        newtxhash = latesttx["transaction_hash"]
+        if newtxhash == txhash:
+            del craft_log
+            del logdata
+            del json_logdata
+            gc.collect()
+            
+            time.sleep(10)
+            check_old()
+        else:
+            getlog()
 
-    
-#Tweets the sale
 def tweet():
     stramount = str(amount)
     text = "Claw: " + strtokenid + " was just sold for " + stramount + " ICX" + "\n" + url
     
     response = client.create_tweet(text=text)
-
     print(response)
+    
     check_old()
 
-getlog()
 
+
+getlog()
