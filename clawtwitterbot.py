6e8836e7ef7cf8bf3bd9e5a383616c4af270095d
sazern <83553077+sazern@users.noreply.github.com>
2024-04-01 21:35:07 +0200

    Update clawtwitterbot.py

--- clawtwitterbot.py
+++ clawtwitterbot.py
@@ -1,10 +1,6 @@
 import requests
 import json
 import time
-from rich.console import Console
-from rich.table import Table
-from rich.progress import track
-from rich import print
 from iconsdk.icon_service import IconService
 from iconsdk.providers.http_provider import HTTPProvider
 from datetime import datetime
@@ -28,13 +24,10 @@ EXA = 10**18
 def getlog():
     try:
         craft_log = requests.get('https://tracker.icon.community/api/v1/logs?address=cx45cf0c108c3df650b1c28d9e2fdc8b4d068cb2fa&method=TransferSingle&limit=1', timeout=(10, 10))
-        print("Connected to ICON API")   
+         
     except:
-        print("[bright_yellow]Timeout to ICON API[/bright_yellow]")
-        for step in track(range(100), description="Checking again soon"):
-            time.sleep(0.2)
+        time.sleep(20)
         getlog()
-        
     logdata = craft_log.text
     json_logdata = json.loads(logdata)
     latesttx = json_logdata[0]
@@ -52,10 +45,7 @@ def getlog():
     
     #Making sure the Operator is Craft contract to be sure its sold on craft network
     if operator == "cx9c4698411c6d9a780f605685153431dcda04609f":
-        sale = 1
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        print(current_time + "[bright_magenta] ### NFT SOLD ###[/bright_magenta]")
+        sale = 1      
      
         
     else:
@@ -65,18 +55,16 @@ def getlog():
         gettxresult()
         
     else:
-           print("[bold blue]Operator is not craft contract[bold blue]")
-           op_not_craft()
+           
+        op_not_craft()
 
 
     
     
 # Lets get the TX details to get tokenID and the Value that it was sold for 
 def gettxresult():
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
+    
     txresult = icon_service.get_transaction_result(txhash)
-    print("lastTX: " + txhash)
     result3 = txresult["eventLogs"][0]
     global status
     status = result3["indexed"][0]
@@ -84,6 +72,7 @@ def gettxresult():
     if status != "ICXTransfer(Address,Address,int)":
         status_nosale()
     else:
+        
         result2 = txresult["eventLogs"][1]
         royhexamount = result2["indexed"][3]
         roydec = int(royhexamount, 16) / EXA
@@ -102,72 +91,43 @@ def gettxresult():
         amount = exaamount / EXA
         stramount = str(amount)
         try:
-            data = result2["data"]
             hextokenid = result2["data"][0]
             global tokenid
             tokenid = int(hextokenid, 16)
             global strtokenid
             strtokenid = str(tokenid)
-            print(strtokenid)
         except:
-            print("[bright_yellow]Couldnt get TokenID from TX[/bright_yellow]")
-            print("[bright_yellow]data in eventlog: [/bright_yellow]", data)
-            for step in track(range(100), description="Checking again soon"):
-                time.sleep(0.2)
+            time.sleep(20)
             gettxresult()
 
-        rarity_url = "https://api.rarityhunter.network/v1/api/nft?contractAddress=cx45cf0c108c3df650b1c28d9e2fdc8b4d068cb2fa&tokenId=" + strtokenid
-        rarity_apiurl = rarity_url
-        rarity_api = requests.get(rarity_apiurl)
-        rarity_data = rarity_api.text
-        json_rarity = json.loads(rarity_data)
-        rarity_rank = json_rarity["data"]["rank"]
-        global strrank
-        strrank = str(rarity_rank)
-        
+#        rarity_url = "https://api.rarityhunter.network/v1/api/nft?contractAddress=cx45cf0c108c3df650b1c28d9e2fdc8b4d068cb2fa&tokenId=" + strtokenid
+#        rarity_apiurl = rarity_url
+#        rarity_api = requests.get(rarity_apiurl)
+#        rarity_data = rarity_api.text
+#        json_rarity = json.loads(rarity_data)
+#        rarity_rank = json_rarity["data"]["rank"]
+#        global strrank
+#        strrank = str(rarity_rank)
+# 	rank = 0
+#	strrank = str(rank)
+       
         global url
         url = "https://craft.network/nft/cx45cf0c108c3df650b1c28d9e2fdc8b4d068cb2fa:" + strtokenid
-        #Prints the details as a table of the NFT that was sold
-        console = Console()
-        table = Table(show_header=True, header_style="bold magenta")
-        table.add_column("From:", style="dim")
-        table.add_column("To:") 
-        table.add_column("Operator", justify="right")
-        table.add_column("TX Hash:", justify="right")
-        table.add_column("Time:", justify="right")
-        table.add_row(
-        seller, reciever, operator, txhash, current_time
-        ) 
-        console.print(table)
-        table2 = Table(show_header=True, header_style="bold magenta")
-        table2.add_column("Value:", style="dim")
-        table2.add_column("Royalty:", style="dim")
-        table2.add_column("TokenID:") 
-        table2.add_column("URL:", justify="right")
-        table2.add_column("Rank:", justify="right")
-        table2.add_column("Status:", justify="right")
-        table2.add_column("Royalty Balance:", justify="right")
-        table2.add_row(
-        stramount, roystramount, strtokenid, url, strrank, status, strbalance
-        ) 
-        console.print(table2)
+        
         #Lets send a tweet about it!
         tweet()
 
 # Checks the api for new sale if it wasnt a direct purchase
 def status_nosale():
-    print("[bold blue] Last TX was not a direct purchase [/bold blue]")
+    
     time.sleep(5)
     global status
     while status != "ICXTransfer(Address,Address,int)":
-        print("Gettning new data")
+        
         try:
             s_craft_log = requests.get('https://tracker.icon.community/api/v1/logs?address=cx45cf0c108c3df650b1c28d9e2fdc8b4d068cb2fa&method=TransferSingle&limit=1', timeout=(10, 10))
-            print("Connected to ICON API")
         except:
-            print("[bright_yellow]Timeout to ICON API[/bright_yellow]")
-            for step in track(range(100), description="Checking again soon"):
-                time.sleep(0.2)
+            time.sleep(20)
             status_nosale()
         s_logdata = s_craft_log.text
         s_json_logdata = json.loads(s_logdata)
@@ -177,9 +137,9 @@ def status_nosale():
         txresult = icon_service.get_transaction_result(txhash)
         s_result3 = txresult["eventLogs"][0]
         status = s_result3["indexed"][0]
-        print("Status: " + status)
-        for step in track(range(100), description="Checking again soon"):
-                time.sleep(0.2)
+        
+        
+        time.sleep(20)
         
     else:
         print("Status:" + status)
@@ -192,13 +152,9 @@ def op_not_craft():
     while operator != "cx9c4698411c6d9a780f605685153431dcda04609f":
         try:
             d_craft_log = requests.get('https://tracker.icon.community/api/v1/logs?address=cx45cf0c108c3df650b1c28d9e2fdc8b4d068cb2fa&method=TransferSingle&limit=1', timeout=(10, 10))
-            print("Connected to ICON API")
         except:
-            print("[bright_yellow]Timeout to ICON API[/bright_yellow]")
-            for step in track(range(100), description="Checking again soon"):
-                time.sleep(0.2)
+            time.sleep(20)
             op_not_craft()
-
         d_logdata = d_craft_log.text
         d_json_logdata = json.loads(d_logdata)
         d_latesttx = d_json_logdata[0]          
@@ -206,83 +162,64 @@ def op_not_craft():
         d_json_indexed = json.loads(d_indexed)
         operator = d_json_indexed[1]
         strsale = str(sale)
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        print(current_time)
-        print("######################################")
-        print("Sale var: " + strsale)
-        print("Operator is not craft contract")
-        print("Operator: " + operator)
-        print("######################################")
       
-        for step in track(range(100), description="Checking again soon"):
-                time.sleep(0.2)
+      
+        
+        time.sleep(20)
     else:
         sale = 1
         getlog()
 
 # Checks the latest transaction for a new sale, If the last tx is the same, then it checks it again 
 def check_old():
+    
     try:
         craft_log = requests.get('https://tracker.icon.community/api/v1/logs?address=cx45cf0c108c3df650b1c28d9e2fdc8b4d068cb2fa&method=TransferSingle&limit=1', timeout=(10, 10))
-        print("Connected to ICON API")
         logdata = craft_log.text
         json_logdata = json.loads(logdata)
     except:
-        now = datetime.now()
-
-        current_time = now.strftime("%H:%M:%S")
-        print(current_time + " couldnt read icon API")
-        for step in track(range(100), description="Checking again soon"):
-                time.sleep(0.2)
+        time.sleep(20)
         check_old()
-    else:
-        latesttx = json_logdata[0]
-        newtxhash = latesttx["transaction_hash"]
-        while newtxhash == txhash:
-            now = datetime.now()
-            current_time = now.strftime("%H:%M:%S")
-            print(current_time + "[yellow] ## No new sold items ## [/yellow]")
-            try:
-                new_log = requests.get('https://tracker.icon.community/api/v1/logs?address=cx45cf0c108c3df650b1c28d9e2fdc8b4d068cb2fa&method=TransferSingle&limit=1', timeout=(10, 10))
-                print("Connected to ICON API")
-            except:
-                now = datetime.now()
-
-                current_time = now.strftime("%H:%M:%S")
-                print(current_time + " couldnt read icon API")
-                for step in track(range(100), description="Checking again soon"):
-                    time.sleep(0.2)
-                    check_old()
-             
-
-            newlogdata = new_log.text
-            json_newlogdata = json.loads(newlogdata)
-            newlatesttx = json_newlogdata[0]
-            newtxhash = newlatesttx["transaction_hash"]
-            print("[bold blue] Last Transaction: [/bold blue]" + newtxhash)
+    latesttx = json_logdata[0]
+    newtxhash = latesttx["transaction_hash"]
+    
+    while newtxhash == txhash:  
+                
+        try:
+            new_log = requests.get('https://tracker.icon.community/api/v1/logs?address=cx45cf0c108c3df650b1c28d9e2fdc8b4d068cb2fa&method=TransferSingle&limit=1', timeout=(10, 10))
+        except:
+            time.sleep(20)
+            check_old()
+        newlogdata = new_log.text
+        json_newlogdata = json.loads(newlogdata)
+        newlatesttx = json_newlogdata[0]
+        newtxhash = newlatesttx["transaction_hash"]
             
-            for step in track(range(100), description="Checking again soon"):
-                time.sleep(0.05)
-        else:
-            getlog()
+            
+        
+        time.sleep(5)
+    else:
+        getlog()
 # Tweets the sale
 def tweet():
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     stramount = str(amount)
-    text = "Claw: " + strtokenid + " was just sold for " + stramount + " $ICX" + " and " + roystramount + " $ICX" + " in royalties" + "\n" + "Rarity Rank: " + strrank + "\n" + "\n" + "Current balance of Claw Royalty wallet is: " + strbalance + " ICX" + "\n" + url
+    text = "Claw: " + strtokenid + " was just sold for " + stramount + " $ICX" + " and " + roystramount + " $ICX" + " in royalties" + "\n" + "\n" + "Current balance of Claw Royalty wallet is: " + strbalance + " ICX" + "\n" + url
+
     try:
         response = client.create_tweet(text=text)
-        print(current_time + "[bright_green] Tweet: [/bright_green]" + f"https://twitter.com/clawnftsales/status/{response.data['id']}")
-        print("[bright_magenta]###Tweet posted!###[bright_magenta]" + "\n" + "\n")
+        print(current_time + " Tweet: " + f"https://twitter.com/clawnftsales/status/{response.data['id']}")
+        
+        
     
         check_old()
     except:
-        print("[bright_yellow]WARN: Couldn't Tweet, Checking for new sale[/bright_yellow]")
+        print("Dublicated tweet, Checking for new TX")
         check_old()
 
 
 print("Starting Bot")
 getlog()
+
 
